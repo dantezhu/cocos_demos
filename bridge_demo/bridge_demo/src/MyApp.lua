13ce8e9b1f8182234a17be7d9014f1debc52fe58
dantezhu <dantezhu@qq.com>
2015-03-17 19:06:46 +0800

    搞定

--- bridge_demo/bridge_demo/src/MyApp.lua
+++ bridge_demo/bridge_demo/src/MyApp.lua
@@ -2,12 +2,25 @@ require("Constants")
 
 
 local M = class("MyApp", neon.Neon)
+local luaoc = require('cocos.cocos2d.luaoc')
+local device = require("cocos.framework.device")
 
 function M:onCreate()
     neon.logger:setLevel(neon.Log.DEBUG)
 
-    self:registerView(require("views.MainView"))
+    local function callback(src)
+        neon.logd("callback src: %s", src)
+    end
 
+    if device.platform == 'ios' then
+        luaoc.callStaticMethod("AppController", "doSomething", {callback = callback})
+        luaoc.callStaticMethod("AppController", "registerStateChangeCallback", {callback = function (state)
+            -- 按下home键切到后台再回来，状态顺序为: inactive enterback enterfore active
+            neon.logd("state callback: %s", state)
+        end})
+    end
+
+    self:registerView(require("views.MainView"))
     self:showView("MainView")
 end
 
--- bridge_demo/bridge_demo/src/views/MainView.lua
+++ bridge_demo/bridge_demo/src/views/MainView.lua
@@ -1,23 +1,12 @@
 local M = class("MainView", neon.View)
 
 function M:onCreate()
-    local device = require("device")
     neon.loge("onCreate %s", self.__cname)
-    neon.loge("platform: %s", device.platform)
     local layer = cc.LayerColor:create(cc.c4b(0,0,255,255))
     self.root:addChild(layer)
 
-    neon.loge("root.contentSize: %s, %s", self.root:getContentSize().width, self.root:getContentSize().height)
-    neon.loge("layer.contentSize: %s, %s", layer:getContentSize().width, layer:getContentSize().height)
-
-    local function callback(src)
-        neon.logd("callback src: %s", src)
-    end
-
-    if device.platform == 'ios' then
-        local luaoc = require('cocos.cocos2d.luaoc')
-        luaoc.callStaticMethod("AppController", "doSomething", {callback = callback})
-    end
+    -- neon.loge("root.contentSize: %s, %s", self.root:getContentSize().width, self.root:getContentSize().height)
+    -- neon.loge("layer.contentSize: %s, %s", layer:getContentSize().width, layer:getContentSize().height)
 end
 
 function M:onRemove()
